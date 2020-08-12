98d042c6c186763ad65230d8a7935b0082a40c37
ValiPython <69545570+ValiPython@users.noreply.github.com>
2020-08-12 23:30:53 +0300

    Update E2-1.py

--- Chapter 2/E2-1.py
+++ Chapter 2/E2-1.py
@@ -1,6 +1,6 @@
-#The volume of a sphere with radius r is 4/3 π r3. What is the volume of a sphere with radius 5?
+# "The volume of a sphere with radius r is 4/3 π r3. What is the volume of a sphere with radius 5?"
 
-#π (pi) = 3.14159265359
+# π (pi) = 3.14159265359
 
 volume = 4 / 3 * 3.14159265359 * (5 ** 3)
 
