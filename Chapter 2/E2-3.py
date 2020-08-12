521c3d038f4d1462e5c6e7858ca9f43de3d8db5a
ValiPython <69545570+ValiPython@users.noreply.github.com>
2020-08-12 23:34:48 +0300

    Update E2-3.py

--- Chapter 2/E2-3.py
+++ Chapter 2/E2-3.py
@@ -1,3 +1,6 @@
+# "If I leave my house at 6:52 am and run 1 mile at an easy pace (8:15 per mile), then 3 miles at tempo (7:12 per mile) and 1 mile at easy pace again,
+#  what time do I get home for breakfast?" 
+
 hour = 6
 
 minutes = 52
