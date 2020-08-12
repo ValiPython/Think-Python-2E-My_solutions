dec7c1b2f94a5250f9f3ec8c01a29a95c4fc615f
ValiPython <69545570+ValiPython@users.noreply.github.com>
2020-08-12 23:37:16 +0300

    Update E2-3.py

--- Chapter 1/E2-3.py
+++ Chapter 1/E2-3.py
@@ -1,4 +1,5 @@
-# "If you run a 10 kilometer race in 42 minutes 42 seconds, what is your average pace (time per mile in minutes and seconds)? What is your average speed in miles per hour?"
+# "If you run a 10 kilometer race in 42 minutes 42 seconds, what is your average pace (time per mile in minutes and seconds)?
+#  What is your average speed in miles per hour?"
 
 km_to_m  = 10 / 1.61
 
