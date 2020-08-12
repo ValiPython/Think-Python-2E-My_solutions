e54773a1ffbd682e4b01fa58b47d8668a2924cfa
ValiPython <69545570+ValiPython@users.noreply.github.com>
2020-08-12 20:19:20 +0300

    Add files via upload

---
+++ E2-1.py
@@ -0,0 +1,5 @@
+# How many seconds are there in 42 minutes 42 seconds ?
+
+time = 42 * 60 + 42
+
+print(time)
---
+++ E2-2.py
@@ -0,0 +1,5 @@
+# How many miles are there in 10 km ? Hint: 1 mile = 1.61 km
+
+km_to_m  = 10 / 1.61
+
+print (km_to_m)
